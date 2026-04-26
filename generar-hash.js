9ef0feed12d6301e9d8998517bbf5078eb99f03f
rod-munoz <rodrigomunozvargas@hotmail.com>
2026-04-26 04:51:38 -0400

    Eliminación archivo temporal generar-hash.js

--- generar-hash.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const generarHash = async () => {
-    const hash = await bcrypt.hash('password123', 10);
-    console.log('Hash generado:', hash);
-};
-
-generarHash();
